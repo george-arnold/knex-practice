f0fec3fec0f4a926b9967d2a40d63e2b4832b73e
George Arnold <george.arnold.88@gmail.com>
2020-02-15 08:45:13 -0500

    created blogful sql added sql entries

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,15 @@
+BEGIN;
+INSERT INTO blogful_articles (title, date_published, content )
+VALUES
+('Fantasy Valentines + Mailbag Madness, Touchdown Music,' '2019-01-16 12:00:00', 'The Fantasy Footballers Podcast is back with a new episode right before Valentine’s Day!'  ),
+('Shocking Stats from 2019 + Buy or Sell','2019-01-16','The Fantasy Footballers Podcast is back! Join us as we take a look back at some shocking stats from the 2019 season' ),
+('2019 Rookie Review Show + MAN LUNGS', '2019-01-16', 'The Fantasy Footballers Podcast is back with a new show reviewing the fantasy seasons of the 2019 NFL rookie class! Just how surprisingly good were rookie WRs for fantasy football? '),
+('Fantasy Footballers Podcast Recap: The TRUTH About Tight Ends in 2019','2019-01-16','Many times during 2019, I referred to the Tight End position in fantasy football as a wasteland and I stand by those words. If you didnt own Travis Kelce or George Kittle, chances are you were disappointed'),
+('The TRUTH: Fantasy TEs in 2019 + Super Bowl Reactions','2019-01-16', 'What a game! The Fantasy Footballers are back to recap Super Bowl 54 and plenty more.' ),
+('25 TE Statistics from 2019 (Fantasy Football)', '2019-01-16', 'Before we get to the 25 tight end statistics, let’s take a look at the tight end position from the perspective of the fantasy draft.  '),
+('The TRUTH: Second Tier QBs in 2019', '2019-01-16', 'The Fantasy Footballers Podcast is back with more TRUTH about quarterbacks from the 2019 fantasy football season! '),
+('Fantasy Footballers Podcast Recap: The TRUTH about Top Tier Fantasy QBs in 2019','2019-01-16','This week the 2019 Truth series shifts to arguably the most misunderstood position in fantasy football, QBs. In their latest episode, The Ballers dove into QB scoring' ),
+('The TRUTH: Top Tier Fantasy QBs in 2019 + Super Bowl Buy/Sell', '2019-01-16', 'The Fantasy Footballers Podcast is back with the TRUTH about the top fantasy football QBs from 2019!' ),
+('25 QB Statistics from 2019 (Fantasy Football)', '2019-01-16','For the fourth year in a row, our 25 Statistics series recaps each position with some of the most meaningful and eye-popping statistics of the year.')
+;
+COMMIT;
